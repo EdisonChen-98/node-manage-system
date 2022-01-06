d1a6c75fcdb1b659cb5614e8bde1f13e2da5a3ff
Yizhou Chen <Yizhou.Chen@lingyue-digital@com>
2022-01-06 16:41:17 +0800

    feat:login接口

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
+/*
+ * @Author: Edison Chen
+ * @Date: 2022-01-06 10:03:02
+ */
 const express = require('express')
-const app=express()
+const app = express()
 
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
-app.get('/test', function(req, res){
-    res.send('hello world');
+app.post('/auth/login', function (req, res) {
+  console.log('login', req.body)
+  res.send({
+    status: 0,
+    data: {
+      username: req.body.username,
+      password: req.body.password
+    }
   });
-app.listen(3007,()=>{
-    console.log('server running at http:127.0.0.1:3007');
+});
+app.listen(3007, () => {
+  console.log('server running at http:127.0.0.1:3007');
 })
