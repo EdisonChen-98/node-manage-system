901c52431200cc35c2dd97b9b5c9090b15ded5a4
Edison_Chen <2508241971@qq.com>
2022-01-10 00:36:17 +0800

    feat:jwt解析token中间件

--- app.js
+++ app.js
@@ -4,30 +4,37 @@
  */
 const express = require('express')
 const joi = require('joi')
+const expressJWT = require('express-jwt')
+const { jwtSecretKey } = require('./config')
 const app = express()
 
 //中间件
 app.use(express.json()) // 解析 application/json
 app.use(express.urlencoded({ extended: true })) // 解析 application/x-www-form-urlencoded
-app.use((req,res,next)=>{
-  res.sendInfo=(error,status=1)=>{
+app.use((req, res, next) => {
+  res.sendInfo = (error, status = 1) => {
     res.send({
       status,
-      message:error instanceof Error?error.message:error
+      message: error instanceof Error ? error.message : error
     })
   }
   next()
 })
+app.use(expressJWT({ secret: jwtSecretKey }).unless({ path: [/^\/common\//] })) //解析 token
+
 
 
 //路由模块
 const commonRouter = require('./router/common')
-app.use('/common',commonRouter)
+app.use('/common', commonRouter)
 
 //错误级别中间件
-app.use((error,req,res,next)=>{
-  if(error instanceof joi.ValidationError){
+app.use((error, req, res, next) => {
+  if (error instanceof joi.ValidationError) {
     return res.sendInfo(error)
+  }
+  if (error.name === 'UnauthorizedError') {
+    return res.sendInfo('身份认证失败')
   }
   res.sendInfo(error)
 })
